cccfbb483b0cbf3404bf3ae8868ec51ece6bae38
ksg1227 <ksg001227@naver.com>
2024-03-19 17:59:33 +0900

    all clear

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,5 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-
-        System.out.println("feature a change");
-
-        System.out.println("feature b 좀 되라");
-
-        System.out.println("feature c change");
-
-        System.out.println("feature d change");
     }
 }
